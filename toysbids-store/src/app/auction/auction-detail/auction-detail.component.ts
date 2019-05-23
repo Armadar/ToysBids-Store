4f00d143da09be65abf9be14438e3dda888bacf9
Jesus Alfredo Alviño Lescano <jalvino@belatrix.com>
2019-05-23 15:31:16 -0500

    Set Routing between the Auction list and detail

--- toysbids-store/src/app/auction/auction-detail/auction-detail.component.ts
+++ toysbids-store/src/app/auction/auction-detail/auction-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-auction-detail',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auction-detail.component.css']
 })
 export class AuctionDetailComponent implements OnInit {
+  id: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      /*
+      this.platoService.getPlato(this.id).subscribe(data => {
+        this.plato = data;
+      });
+      */
+    });
   }
-
 }
